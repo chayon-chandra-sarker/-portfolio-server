acebe95d4e6cc3ea667c47f0037bb3d706d41398
chayon <chayon438@gmail.com>
2026-07-05 01:54:50 +0600

    problem solved

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,4 @@
 import "dotenv/config";
-import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
 
-const connectionString = process.env.DATABASE_URL!;
-
-const adapter = new PrismaPg({ connectionString });
-
-export const prisma = new PrismaClient({
-  adapter,
-});
+export const prisma = new PrismaClient();
