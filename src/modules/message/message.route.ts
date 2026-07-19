48bb12644c3d65a03d463ae207d3adf037aae44e
chayon <chayon438@gmail.com>
2026-07-19 22:56:43 +0600

    update message api

--- src/modules/message/message.route.ts
+++ src/modules/message/message.route.ts
@@ -4,9 +4,9 @@ import { auth } from "../../middleware/auth";
 
 const router = Router();
 
-router.post("/create", auth("ADMIN"), messageController.createMessage);
-router.get("/all-messages", messageController.getAllMessages);
-router.get("/single/:id", messageController.getSingleMessage);
+router.post("/create", messageController.createMessage);
+router.get("/all-messages", auth("ADMIN"), messageController.getAllMessages);
+router.get("/single/:id", auth("ADMIN"), messageController.getSingleMessage);
 router.patch("/update/:id", auth("ADMIN"), messageController.updateMessage);
 router.delete("/delete/:id", auth("ADMIN"), messageController.deleteMessage);
 
